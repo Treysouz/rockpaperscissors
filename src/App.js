1a4620fe46b31ec406d377e314b855f314550266
Tremayne Souza <treysouz@gmail.com>
2020-02-25 20:57:06 -0500

    Second Commit - Added Basic Functions for CPU RPS Game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PlayerCard from "./PlayerCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Game extends Component {
+  constructor() {
+    super();
+    this.signs = ["rock", "scissors", "paper"];
+    this.state = {
+      playerOne: "",
+      playerTwo: "",
+      winner: "",
+      results: ""
+    };
+  }
+  playGame = () => {
+    this.setState({
+      playerOne: this.signs[Math.floor(Math.random() * 3)],
+      playerTwo: this.signs[Math.floor(Math.random() * 3)]
+    });
+  };
+
+  decideWinner = () => {
+    if (
+      this.state.playerOne === "rock" &&
+      this.state.playerTwo === "scissors"
+    ) {
+      this.setState({
+        winner: "Player One Wins!",
+        results: "Rock beats Scissors"
+      });
+    } else if (
+      this.state.playerTwo === "rock" &&
+      this.state.playerOne === "scissors"
+    ) {
+      this.setState({
+        winner: "Player Two Wins!",
+        results: "Rock beats Scissors"
+      });
+    } else if (
+      this.state.playerTwo === "paper" &&
+      this.state.playerOne === "scissors"
+    ) {
+      this.setState({
+        winner: "Player Two Wins!",
+        results: "Scissors beats Paper"
+      });
+    } else if (
+      this.state.playerOne === "paper" &&
+      this.state.playerTwo === "scissors"
+    ) {
+      this.setState({
+        winner: "Player One Wins!",
+        results: "Scissors beats Paper"
+      });
+    } else if (
+      this.state.playerOne === "rock" &&
+      this.state.playerTwo === "paper"
+    ) {
+      this.setState({
+        winner: "Player Two Wins!",
+        results: "Paper beats Rock"
+      });
+    } else if (
+      this.state.playerTwo === "rock" &&
+      this.state.playerOne === "paper"
+    ) {
+      this.setState({
+        winner: "Player Two Wins!",
+        results: "Paper beats Rock"
+      });
+    } else if (this.state.playerTwo === this.state.playerOne) {
+      this.setState({
+        winner: "No Winner",
+        results: "It's a Draw!"
+      });
+    } else {
+      this.setState({
+        winner: "No Winner",
+        results: "There was an Error"
+      });
+    }
+  };
+
+  render() {
+    return (
+      <main>
+        <section className="playCardContainer">
+          <PlayerCard sign={this.state.playerOne}></PlayerCard>
+          <PlayerCard sign={this.state.playerTwo}></PlayerCard>
+        </section>
+        <section className="playInfoContainer">
+          <p>{this.state.results}</p>
+          <p>{this.state.winner}</p>
+        </section>
+        <section className="playBtnContainer">
+          <button type="button" onClick={this.playGame}>
+            Play the Game
+          </button>
+        </section>
+      </main>
+    );
+  }
 }
-
-export default App;
---
+++ src/PlayerCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlayerCard = (props) =>{
+    return(
+        <div className="playerCard">{props.sign}</div>
+    )
+
+}
+
+export default PlayerCard;
